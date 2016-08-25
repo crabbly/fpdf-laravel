1b940d45633e1c2ae75afedf7c839155628bc703
Rodrigo Vieira <rodrigo@crabbly.com>
2016-08-25 17:16:02 -0400

    first commit

---
+++ src/FpdfServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Crabbly\FPDF;
+
+use Illuminate\Support\ServiceProvider;
+
+class FpdfServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
